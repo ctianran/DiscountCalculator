79527340c27f583c101997bdbed8139eaf3da498
ctianran <tristan20177@gmail.com>
2017-06-23 19:40:36 -0700

    Initial Commit

---
+++ DiscountCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiscountCalculator
+//
+//  Created by Tianran Chen on 6/23/17.
+//  Copyright © 2017 Tianran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiscountCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DiscountCalculator
+//
+//  Created by Tianran Chen on 6/23/17.
+//  Copyright © 2017 Tianran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
